80cf405f2c7814a190f4025d8b867fdb7f4f2b7f
Luke Olney <lukeolney@gmail.com>
2018-04-07 02:54:56 -0700

    Fix splitting of tags in readmore.js

--- js/readmore.js
+++ js/readmore.js
@@ -1,24 +1,45 @@
 function findSplit(children, limit) {
     var text = children[0].innerHTML;
-    var newfirst = $("<p></p>").append(text.substring(0, limit));
-    children.first().text(text.substring(limit)); // sets as blank if longer than limit
+    var alimit = adjustLimit(text, limit);
+    var newfirst = $("<p></p>").append(text.substring(0, alimit));
+    children.first().text(text.substring(alimit)); // sets as blank if longer than limit
     return newfirst;
 }
 
+function adjustLimit(text, limit) {
+    var effective_length = 0;
+    var total_length = 0;
+    for (const token of text.split(/(<.+>)/)) {
+        switch (token[0]) {
+            case '<':
+                total_length += token.length
+            default:
+                for (const char in token.split("")) {
+                    if (effective_length == limit)
+                        return total_length;
+                    effective_length++;
+                    total_length++;
+                }
+        }
+    }
+    return total_length;
+}
+
 function moreLess(initiallyVisibleCharacters) {
-    var visibleCharacters = initiallyVisibleCharacters;
+    var limit = initiallyVisibleCharacters;
     var paragraph = $(".readmore")
 
 
     paragraph.each(function () {
         var children = $(this).children();
-        var allText = children.get().reduce((begin, child) => begin + child.innerHTML, "");
+        // Only count inner text
+        var allText = children.get().reduce((begin, child) => begin + adjustLimit(child.innerHTML, limit), 0);
 
-        if (allText.length < visibleCharacters || (children.length < 1)) {
+        if (allText < limit || (children.length < 1)) {
             return
         } else {
             var oldChildren = children.detach();
-            var first = findSplit(oldChildren, visibleCharacters);
+            var first = findSplit(oldChildren, limit);
             var more = $("<span class='ellipsis'>... </span><a href='#' class='more'>MORE<i class='fa fa-arrow-circle-o-down' aria-hidden='true'></i></a>");
             var rest = oldChildren.splice(1);
             var text = oldChildren[0].innerHTML;
